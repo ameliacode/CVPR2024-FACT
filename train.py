ffce0d5b25772cdc1a905c9a7aa568ac0d389e3a
ameliacode <ameliacode@hanyang.ac.kr>
2025-07-02 14:35:18 +0900

    [Task] Fix train code

--- train.py
+++ train.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 import argparse
 import json
 import os
@@ -9,7 +7,7 @@ import torch
 from torch import optim
 from tqdm import tqdm
 
-from configs.utils import cfg2flatdict, setup_cfg
+from configs.utils import setup_cfg
 from home import get_project_base
 from models.loss import MatchCriterion
 from utils.dataset import DataLoader, create_dataset
@@ -18,67 +16,34 @@ from utils.train_tools import compute_null_weight, resume_ckpt, save_results
 
 
 def evaluate(global_step, net, testloader, savedir):
-    print("TESTING" + "~" * 10)
-
     ckpt = Checkpoint(
         global_step + 1,
         bg_class=([] if net.cfg.eval_bg else testloader.dataset.bg_class),
     )
     net.eval()
     with torch.no_grad():
-        for batch_idx, (
-            vnames,
-            seq_list,
-            train_label_list,
-            eval_label_list,
-        ) in enumerate(testloader):
-
+        for vnames, seq_list, train_label_list, eval_label_list in testloader:
             seq_list = [s.cuda() for s in seq_list]
             train_label_list = [s.cuda() for s in train_label_list]
             video_saves = net(seq_list, train_label_list)
             save_results(ckpt, vnames, eval_label_list, video_saves)
-
     net.train()
     ckpt.compute_metrics()
-
-    string = ""
-    for k, v in ckpt.metrics.items():
-        string += "%s:%.1f, " % (k, v)
-    print(string + "\n")
-
-    fname = "%d.gz" % (global_step + 1)
-    ckpt.save(os.path.join(savedir, fname))
-
+    ckpt.save(os.path.join(savedir, f"{global_step + 1}.pth"))
     return ckpt
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--cfg", dest="cfg_file", nargs="*", default=[])
     parser.add_argument(
-        "--cfg", dest="cfg_file", nargs="*", help="optional config file", default=[]
+        "--set", dest="set_cfgs", default=None, nargs=argparse.REMAINDER
     )
-    parser.add_argument(
-        "--set",
-        dest="set_cfgs",
-        help="set config keys",
-        default=None,
-        nargs=argparse.REMAINDER,
-    )
-
     args = parser.parse_args()
+
     BASE = get_project_base()
-
-    ### initialize experiment #########################################################
     cfg = setup_cfg(args.cfg_file, args.set_cfgs)
-    try:
-        torch.cuda.set_device("cuda:%d" % cfg.aux.gpu)
-    except Exception as e:
-        print(e)
-        os.environ["CUDA_VISIBLE_DEVICES"] = str(cfg.aux.gpu)
-
-    print("============")
-    print(cfg)
-    print("============")
+    torch.cuda.set_device("cuda:%d" % cfg.aux.gpu)
 
     if cfg.aux.debug:
         seed = 1
@@ -90,26 +55,18 @@ if __name__ == "__main__":
     logdir = os.path.join(BASE, cfg.aux.logdir)
     ckptdir = os.path.join(logdir, "ckpts")
     savedir = os.path.join(logdir, "saves")
-    os.makedirs(logdir, exist_ok=True)
     os.makedirs(ckptdir, exist_ok=True)
     os.makedirs(savedir, exist_ok=True)
-    print("Saving log at", logdir)
 
-    argSaveFile = os.path.join(logdir, "args.json")
-    with open(argSaveFile, "w") as f:
+    with open(os.path.join(logdir, "args.json"), "w") as f:
         json.dump(cfg, f, indent=True)
 
-    ### load dataset #########################################################
     dataset, test_dataset = create_dataset(cfg)
-    if not cfg.aux.debug:
-        trainloader = DataLoader(dataset, batch_size=cfg.batch_size, shuffle=True)
-    else:
-        trainloader = DataLoader(test_dataset, batch_size=cfg.batch_size, shuffle=True)
-    testloader = DataLoader(test_dataset, batch_size=cfg.batch_size, shuffle=False)
-    print("Train dataset", dataset)
-    print("Test dataset ", test_dataset)
+    trainloader = DataLoader(
+        dataset if not cfg.aux.debug else test_dataset, cfg.batch_size, shuffle=True
+    )
+    testloader = DataLoader(test_dataset, cfg.batch_size, shuffle=False)
 
-    ### create network #########################################################
     if cfg.dataset == "epic":
         from models.blocks_SepVerbNoun import FACT
 
@@ -125,15 +82,14 @@ if __name__ == "__main__":
 
     global_step, ckpt_file = resume_ckpt(cfg, logdir)
     if ckpt_file is not None:
-        ckpt = torch.load(ckpt_file, map_location="cpu")
-        if "frame_pe.pe" in ckpt:
-            del ckpt["frame_pe.pe"]
-        if "action_pe.pe" in ckpt:
-            del ckpt["action_pe.pe"]
-        net.load_state_dict(ckpt, strict=False)
-    net.cuda()
+        checkpoint = torch.load(ckpt_file, map_location="cpu")
+        if "frame_pe.pe" in checkpoint:
+            del checkpoint["frame_pe.pe"]
+        if "action_pe.pe" in checkpoint:
+            del checkpoint["action_pe.pe"]
+        net.load_state_dict(checkpoint, strict=False)
 
-    print(net)
+    net.cuda()
 
     if cfg.optimizer == "SGD":
         optimizer = optim.SGD(
@@ -142,30 +98,19 @@ if __name__ == "__main__":
             momentum=cfg.momentum,
             weight_decay=cfg.weight_decay,
         )
-    elif cfg.optimizer == "Adam":
+    else:
         optimizer = optim.Adam(
             net.parameters(), lr=cfg.lr, weight_decay=cfg.weight_decay
         )
 
-    ### start training #########################################################
     start_epoch = global_step // len(trainloader)
     ckpt = Checkpoint(
-        -1,
-        bg_class=([] if net.cfg.eval_bg else testloader.dataset.bg_class),
-        eval_edit=False,
+        -1, bg_class=([] if cfg.eval_bg else dataset.bg_class), eval_edit=False
     )
     best_ckpt, best_metric = None, 0
 
-    print(f"Start Training from Epoch {start_epoch}...")
     for eidx in tqdm(range(start_epoch, cfg.epoch)):
-
-        for batch_idx, (
-            vnames,
-            seq_list,
-            train_label_list,
-            eval_label_list,
-        ) in enumerate(trainloader):
-
+        for vnames, seq_list, train_label_list, eval_label_list in trainloader:
             seq_list = [s.cuda() for s in seq_list]
             train_label_list = [s.cuda() for s in train_label_list]
 
@@ -175,59 +120,25 @@ if __name__ == "__main__":
 
             if cfg.clip_grad_norm > 0:
                 torch.nn.utils.clip_grad_norm_(net.parameters(), cfg.clip_grad_norm)
-            optimizer.step()
 
+            optimizer.step()
             save_results(ckpt, vnames, eval_label_list, video_saves)
 
-            # print some progress information
-            if (global_step + 1) % cfg.aux.print_every == 0:
-
-                ckpt.compute_metrics()
-                ckpt.average_losses()
-
-                string = "Iter%d, " % (global_step + 1)
-                _L = len(string)
-                for k, v in ckpt.loss.items():
-                    string += f"{k}:{v:.1f}, "
-                print(string)
-
-                string = " " * _L
-                for k, v in ckpt.metrics.items():
-                    string += "%s:%.3f, " % (k, v)
-                print(string)
-
-                ckpt = Checkpoint(
-                    -1,
-                    bg_class=(dataset.bg_class if cfg.eval_bg else []),
-                    eval_edit=False,
-                )
-
-            # test and save model every x iterations
-            if global_step != 0 and (global_step + 1) % cfg.aux.eval_every == 0:
+            if (global_step + 1) % cfg.aux.eval_every == 0:
                 test_ckpt = evaluate(global_step, net, testloader, savedir)
                 if test_ckpt.metrics["F1@0.50"] >= best_metric:
                     best_ckpt = test_ckpt
                     best_metric = test_ckpt.metrics["F1@0.50"]
-
-                network_file = (
-                    ckptdir + "/network.iter-" + str(global_step + 1) + ".net"
-                )
-                net.save_model(network_file)
-
+                    torch.save(net.state_dict(), os.path.join(logdir, "best_ckpt.pth"))
             global_step += 1
 
         if cfg.lr_decay > 0 and (eidx + 1) % cfg.lr_decay == 0:
             for g in optimizer.param_groups:
-                g["lr"] = cfg.lr * 0.1
-            print(
-                "------------------------------------Update Learning rate--------------------------------"
-            )
+                g["lr"] *= 0.1
 
-    print(f"Best Checkpoint: {best_ckpt.iteration}")
-    best_ckpt.eval_edit = True
-    best_ckpt.compute_metrics()
-    best_ckpt.save(os.path.join(logdir, "best_ckpt.gz"))
+    if best_ckpt is not None:
+        best_ckpt.eval_edit = True
+        best_ckpt.compute_metrics()
+        best_ckpt.save(os.path.join(logdir, "best_metrics.pth"))
 
-    # create a file to mark this experiment has completed
-    finish_proof_fname = os.path.join(logdir, "FINISH_PROOF")
-    open(finish_proof_fname, "w").close()
+    open(os.path.join(logdir, "FINISH_PROOF"), "w").close()
